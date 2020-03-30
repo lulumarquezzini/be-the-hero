aed16894eeda1baf761c9f6b4d43cc03e82418c7
Luis Marquezzini <marquezzini@dcc.ufrj.br>
2020-03-30 14:17:08 -0300

    All styles added

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Logon from './pages/logon';
 import Register from './pages/register';
 import Profile from './pages/profile';
+import NewIncident from './pages/newincident'
 
 export default function Routes() {
     return(
@@ -12,6 +13,7 @@ export default function Routes() {
                 <Route exact path="/" component={Logon}/>
                 <Route exact path="/register" component={Register}/>
                 <Route exact path="/profile" component={Profile} />
+                <Route exact path="/incidents/new" component={NewIncident} />
             </Switch>
         </BrowserRouter>
     )
