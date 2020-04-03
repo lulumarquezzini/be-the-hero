8b23a544577b8df85e20d428cf71a0fbad58034c
Luis Marquezzini <marquezzini@dcc.ufrj.br>
2020-04-03 10:09:01 -0300

    Testing tests lol

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,6 +11,4 @@ app.use(routes);
 app.use(errors())
 
 
-app.listen(3333, function () {
-    console.log('App listening on port 3333!');
-  });
+module.exports = app;
---
+++ backend/src/server.js
@@ -0,0 +1,5 @@
+const app = require('./app');
+
+app.listen(3333, function () {
+    console.log('App listening on port 3333!');
+  });
